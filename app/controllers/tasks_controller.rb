aecc23cfaadac17df34776f64c641939e699e08a
Andrew Kofink <ajkofink@gmail.com>
2013-08-29 20:27:58 -0400

    Add tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,13 @@
+class TasksController < ApplicationController
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Utility::Application.routes.draw do
+  get "tasks/create"
+  get "tasks/update"
+  get "tasks/destroy"
+  get "tasks/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

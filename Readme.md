ece47d5fecd2e7336e7344b165e85b43c843144e
Andrew Kofink <ajkofink@gmail.com>
2013-08-29 20:15:50 -0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# My utility app
+
+This app is for my personal benefit. You are welcome to fork it and make it your
+own tool.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Utility::Application.config.secret_key_base = 'b733d3fba4f55260b32f5fb19b4d13ecf8039a372865352db91a2e4fa8e77aa9df05fc6c8209a6c151cf15eff753cd27bd83ed065aef02c59780cd551e6552db'
